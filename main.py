403a8c2a4a18a7e790e7e2e88c81d42258a825e9
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-11-24 18:04:56 -0600

    Init

---
+++ main.py
@@ -0,0 +1,10 @@
+#Filtro de Kalman Unscented
+# Diseño de Sistemas Inteligentes
+# Corte 3
+# Universidad Politécnica de Chiapas
+# Creado por Luis Fernando Hernández Morales y David Pérez Sánchez
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+	print('Hola')
---
+++ utils/Unscented.py
@@ -0,0 +1,7 @@
+class KalmanFilter():
+	"""
+	Clase con el algoritmo de Kalman unscented	
+	"""
+	def __init__(self,):
+		print('Hola bb')
+		
