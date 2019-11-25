a889006998f7bbd7ec618ac3f6f24a0fdaeff607
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-11-24 18:06:23 -0600

    ...

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Corte 3
 # Universidad Politécnica de Chiapas
 # Creado por Luis Fernando Hernández Morales y David Pérez Sánchez
+from utils.Unscented import KalmanFilter
 import matplotlib.pyplot as plt
 import numpy as np
 
