7e26b3c3d090406b5e0780720aba7c189d36e49d
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-12-09 18:53:51 -0600

    Avances

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Corte 3
 # Universidad Politécnica de Chiapas
 # Creado por Luis Fernando Hernández Morales y David Pérez Sánchez
-from utils.Unscented import KalmanUnscented
+from utils.Unscented import ukf
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def dinamica(t):
+def transformada(t):
 	"""
 	Calcular la función de transición del estado ƒ(x),
 	al mismo la función de observación h(x).
@@ -18,120 +18,136 @@ def dinamica(t):
 	Parámetros:
 	No requiere de parámetros.
 	"""
-	# fx = [
-	# 	[ukf.xs[ukf.i][0,0]],
-	# 	[ukf.xs[ukf.i][1,0]],
-	# 	[ukf.xs[ukf.i][2,0]],
-	# 	[ukf.xs[ukf.i][3,0]]
-	# ] # nx1
-	# hx = [
-	# 	[ukf.xs[ukf.i][0,0]],
-	# 	[ukf.xs[ukf.i][1,0]]
-	# ] # mx1 => m = Tamaño de la observación
 	fx = [
-		[ukf.xs[ukf.i][0,0] * math.cos(angulo_lanzamiento) * t],
-		[ukf.xs[ukf.i][1,0] * math.sin(angulo_lanzamiento) * tiempo[i] + ( 1/2 * gravedad * math.pow(tiempo[i], 2) )],
-		[ukf.xs[ukf.i][2,0]],
-		[ukf.xs[ukf.i][3,0]]
-	] # nx1
+		[_ukf.xs[_ukf.i][0,0] + t * _ukf.xs[_ukf.i][1,0]], # Posición en X
+		[_ukf.xs[_ukf.i][1,0]],						   # Velocidad en X
+		[_ukf.xs[_ukf.i][2,0] + t * _ukf.xs[_ukf.i][3,0]], # Posición en Y
+		[_ukf.xs[_ukf.i][3,0]]						   # Velocidad en Y
+	]
 	hx = [
-		[ukf.xs[ukf.i][0,0] * math.cos(angulo_lanzamiento)],
-		[ukf.xs[ukf.i][1,0] * math.sin(angulo_lanzamiento) - gravedad * t]
-	] # mx1 => m = Tamaño de la observación
+		[math.sqrt(_ukf.xs[_ukf.i][0,0]**2 + _ukf.xs[_ukf.i][2,0]**2)], # Distancia del radar
+		[math.atan2(_ukf.xs[_ukf.i][2,0],_ukf.xs[_ukf.i][0,0])]			# Ángulo del radar
+	]
 	return fx, hx
 
+"""
+Variables iniciales para el estado
+"""
+posicion_x_inicial = -2900
+posicion_y_inicial = 950
+velocidad_x_inicial = 80
+velocidad_y_inicial = 20
 
-"""
-Establecer las variables iniciales
-"""
-angulo_lanzamiento = 50  # grados
-velocidad_incial = 5  # m/s => V0
-gravedad = 9.81 # m/s
-x_inicial = 0
-y_inicial = 10
-ruido_R = 0.003
-iteraciones = 25
+ruido_p = 100
+ruido_q = 0.1
+ruido_R = 50
+ruido_V = 0.005
+
+instantes_tiempo = 60
 
 estado = [
-	[x_inicial], # x
-	[y_inicial], # y
-	[0], # vx
-	[velocidad_incial], # vy
-] # nx1
+	[posicion_x_inicial], # Posición en X
+	[velocidad_x_inicial],	 # Velocidad en X
+	[posicion_y_inicial],   # Posición en Y
+	[velocidad_y_inicial]	 # Velocidad en Y
+]
 
 P = [
-	[0.1, 0, 0, 0],
-	[0, 0.1, 0, 0],
-	[0, 0, 0.1, 0],
-	[0, 0, 0, 0.1]
+	[ruido_p, 0, 0, 0],
+	[0, ruido_p, 0, 0],
+	[0, 0, ruido_p, 0],
+	[0, 0, 0, ruido_p]
 ] # nx1
 
 Q = [
-	[0.0001, 0, 0, 0],
-	[0, 0.0001, 0, 0],
-	[0, 0, 0.0001, 0],
-	[0, 0, 0, 0.0001]
+	[0, 0, 0, 0],
+	[0, ruido_q, 0, 0],
+	[0, 0, 0, 0],
+	[0, 0, 0, ruido_q]
 ] # nx1
 
 R = [
-	[np.random.normal(0, ruido_R), 0],
-	[0, np.random.normal(0, ruido_R)]
+	[np.random.normal(0, ruido_R**2), 0],
+	[0, np.random.normal(0, ruido_V**2)]
 ] #mxm => Tamaño de las observaciones
 
 """
 Inicializar la clase del algoritmo de kalman unscented
 """
-ukf = KalmanUnscented()
-ukf.set_x(estado)
-ukf.set_p(P)
-ukf.set_q(Q)
-ukf.set_r(R)
+_ukf = ukf()
+_ukf.setX(estado)
+_ukf.setP(P)
+_ukf.setQ(Q)
+_ukf.setR(R)
+_ukf.setAlpha(0.001)
+_ukf.setBeta(2)
+_ukf.setKappa(0)
+tiempos = np.arange(0, instantes_tiempo, 0.1)
+tiempo_anterior = 0
 
-PROCESO_REAL_X = []
-PROCESO_REAL_Y = []
-FILTRADA_X = []
-FILTRADA_Y = []
-PREDICHA_X = []
-PREDICHA_Y = []
+# Fijar los límites de la gráfica
+x = np.linspace(-3000,3000,len(tiempos))
+y = np.linspace(1000,800,len(tiempos))
+r = np.sqrt(x**2+y**2) # Medida
+theta = np.arctan2(y,x) # Medida
+ruido_r = np.random.normal(0,ruido_R,len(r))
+ruido_theta = np.random.normal(0,ruido_V,len(theta))
+vy = np.gradient(y,tiempos) # Dinámica de la velocidad en X
+vx = np.gradient(x,tiempos) # Dinámica de la velocidad en Y
+_r = r + ruido_r
+_theta = theta + ruido_theta
 
+posicion_x_filtrada = []
+posicion_y_filtrada = []
+velocidad_x_filtrada = []
+velocidad_y_filtrada = []
 
-# Instantes de tiempo
-tiempo = np.arange(1, iteraciones, 1) # Arreglo de 1 hasta iteraciones en saltos de 1
+for i in range(len(tiempos)):
+	t = tiempos[i]-tiempo_anterior
+	tiempo_anterior = tiempos[i]
+	z = [
+		[_r[i]], # Observación del sensor => distancia
+		[_theta[i]] # Observación del sensor => distancia
+	]
+	_ukf.step(z, transformada, t)
+	x_filtrada = _ukf.getX()
+	posicion_x_filtrada.append(x_filtrada[0,0])
+	velocidad_x_filtrada.append(x_filtrada[1,0])
+	posicion_y_filtrada.append(x_filtrada[2,0])
+	velocidad_y_filtrada.append(x_filtrada[3,0])
+
+posicionesX = np.array(posicion_x_filtrada)
+posicionesY = np.array(posicion_y_filtrada)
+velocidadesX = np.array(velocidad_x_filtrada)
+velocidadesY = np.array(velocidad_y_filtrada)
+
 """
-Dinámica del problema
+Graficar
 """
-# x = ( velocidad_incial * math.cos(angulo_lanzamiento) ) * tiempo[0] # x = ( V0 cos α ) * t
-# y = ( velocidad_incial * math.sin(angulo_lanzamiento) * tiempo[0] ) + ( 1/2 * gravedad * math.pow(tiempo[0], 2) ) # y = ( V0 sin α ) * t + 1/2 * g * t^2
-# ruido_dinamica = np.random.normal(0,ruido_R)
-# PROCESO_REAL_X.append(x + ruido_dinamica)
-# PROCESO_REAL_Y.append(y + ruido_dinamica)
 
-for i in range(len(tiempo)):
-	# Proceso real
-	x_ = ( velocidad_incial * math.cos(angulo_lanzamiento) ) * tiempo[i] # x = ( V0 cos α ) * t
-	y_ = ( velocidad_incial * math.sin(angulo_lanzamiento) * tiempo[i] ) + ( 1/2 * gravedad * math.pow(tiempo[i], 2) ) # y = ( V0 sin α ) * t + 1/2 * g * t^2
-	ruido_dinamica = np.random.normal(0,ruido_R)
-	PROCESO_REAL_X.append(x_ + ruido_dinamica)
-	PROCESO_REAL_Y.append(y_ + ruido_dinamica)
-	
-	"""
-	Realizar observaciones Z
-	"""
-	Z = [
-			[ velocidad_incial * math.cos(angulo_lanzamiento) ], #Vx = v0 cos θ0
-			[ velocidad_incial * math.sin(angulo_lanzamiento) - gravedad * tiempo[i] ], #Vy = v0 sin θ0 - g * t
-		]
-
-	ukf.iteracion(Z, dinamica, tiempo[i])
-	_filtrada = ukf.get_filtrada()
-	_predicha = ukf.get_prediccion()
-	FILTRADA_X.append(_filtrada[0,0])
-	FILTRADA_Y.append(_filtrada[1,0])
-	PREDICHA_X.append(_predicha[0,0])
-	PREDICHA_Y.append(_predicha[1,0])
-
-plt.plot(FILTRADA_X, FILTRADA_Y, 'r-', label="Filtrada")
-plt.plot(PREDICHA_X, PREDICHA_Y, 'b-', label="Predicha")
-plt.title('Observaciones del sensor')
+plt.subplot(411)
+plt.plot(tiempos,x,'b',label='Truth')
+plt.plot(tiempos,posicion_x_filtrada,'r',label='UKF')
+plt.title('X position')
+plt.xticks([])
+plt.subplot(412)
+plt.plot(tiempos,vx,'b',label='Truth')
+plt.plot(tiempos,velocidad_x_filtrada,'r',label='UKF')
+plt.title('X velocity')
+plt.xticks([])
+plt.subplot(413)
+plt.plot(tiempos,y,'b',label='Truth')
+plt.plot(tiempos,posicion_y_filtrada,'r',label='UKF')
+plt.title('Y position')
+plt.xticks([])
+plt.subplot(414)
+plt.plot(tiempos,vy,'b',label='Truth')
+plt.plot(tiempos,velocidad_y_filtrada,'r',label='UKF')
+plt.title('Y velocity')
+plt.legend()
+plt.figure()
+plt.plot(x,y,'b',label='Truth')
+plt.plot(posicion_x_filtrada,posicion_y_filtrada,'r',label='UKF')
+plt.title('XY Position')
 plt.legend()
 plt.show()
--- utils/Unscented.py
+++ utils/Unscented.py
@@ -1,176 +1,117 @@
-from scipy import linalg as scipy_alg
 import numpy as np
+from scipy import linalg as scipy_alg
 
+# n = number of states
+# m = number of observations
 
-class KalmanUnscented:
-	"""
-	Algoritmo de Kalman Unscented.
-	Las ecuaciones fueron obtenidas del siguiente Documento:
-	[ http://bibing.us.es/proyectos/abreproy/60353/fichero/Diseño+y+simulación+en+Matlab+de+los+algoritmos+EKF+y+UKF+aplicados+a+la+trayectoria+de+una+aeronave+equipada+con+un+sist~1.pdf ]
-	"""
-	def __init__(self):
-		self.Xs = None # 2L+1 puntos sigma para fx
-		self.Zs = None # 2L+1 puntos sigma para hx
-		self.alpha = 10e-3
-		self.beta = 2
-		self.kappa = 0
-	
-	# Inicializadores de variables
-	def set_x(self, vector_x):
-		"""
-		Actualizar la matriz X.
+class model:
 
-		Parámetros:
-		`vector_x`: Arreglo de tamaño n x 1
-		"""
-		self.x = np.matrix(vector_x)
-	
-	def set_fx(self, matriz):
-		"""
-		Actualizar la matriz de F(x)
-		
-		Parámetros:
-		`matriz`: Arreglo de tamaño n x 1
-		"""
-		self.fx = np.matrix(matriz)
-	
-	def set_hx(self, matriz):
-		"""
-		Actualizar la matriz de H(x)
-		
-		Parámetros:
-		`matriz`: Arreglo de tamaño m x 1
-		"""
-		self.hx = np.matrix(matriz)
-	
-	def set_p(self, matriz):
-		"""
-		Actualizar la matriz de covarianzas
-		
-		Parámetros:
-		`matriz`: Arreglo de tamaño n x n
-		"""
-		self.P = np.matrix(matriz)
-	
-	def set_q(self, matriz):
-		"""
-		Establecer el arreglo Q (ruido)
-		
-		Parámetros:
-		`matriz`: Arreglo de tamaño n x n
-		"""
-		self.Q = np.matrix(matriz)
-	
-	def set_r(self, matriz):
-		"""
-		Establecer el arreglo R (ruido)
-		
-		Parámetros:
-		`matriz`: Arreglo de tamaño m x m
-		"""
-		self.R = np.matrix(matriz)
-	
-	def puntos_sigma(self):
-		"""
-		Realizar la transformada y obtener los puntos sigma
-		"""
-		puntos = []
-		self.pesos_x = []  # Pesos para cada punto sigma del estado
-		self.pesos_p = []  # Pesos para cada punto sigma de la covarianza
-		L = self.x.shape[0]
-		lam = (self.alpha**2)*(L+self.kappa) - L
-		puntos.append(self.x)
-		add_val = scipy_alg.sqrtm((L + lam) * self.P)
-		for i in range(add_val.shape[1]):
-			add_val_mat = np.zeros(self.P.shape[0])
-			for j in range(add_val.shape[0]):
-				add_val_mat[j] = add_val[j][i]
-			add_val_mat = np.matrix(add_val_mat).T
-			puntos.append(self.x + add_val_mat)
-		for i in range(add_val.shape[1]):
-			add_val_mat = np.zeros(self.P.shape[0])
-			for j in range(add_val.shape[0]):
-				add_val_mat[j] = add_val[j][i]
-			add_val_mat = np.matrix(add_val_mat).T
-			puntos.append(self.x - add_val_mat)
-		self.pesos_x.append(lam/(L+lam))
-		self.pesos_p.append((lam/(L+lam)) + (1-self.alpha**2+self.beta))
-		for i in range(2*L):
-			self.pesos_x.append(1/(2*(L+lam)))
-			self.pesos_p.append(1/(2*(L+lam)))
-		self.xs = puntos
-	
-	def actualizar_sigmas(self, funcion, *args):
-		self.puntos_sigma()
-		self.Xs = [] # Xk|k-1
-		self.Zs = [] # Yk
-		for self.i in range(len(self.xs)):
-			Xs_tem, Zs_tem = funcion(*args) # Pasamos el estado y la observación en la función no lineal
-			self.Xs.append(np.matrix(Xs_tem))
-			self.Zs.append(np.matrix(Zs_tem))
+    def __init__(self):
+        self.Xs = None      # 2L+1 sigma points for fx
+        self.Zs = None      # 2L+1 sigma points for hx
+    
+    def setX(self, mat):    # n x 1
+        self.x = np.matrix(mat)
 
-	def prediccion(self):
-		"""
-		Etapa de predicción
-		"""
-		self.xp = 0
-		self.zp = 0  # Zk
-		for i in range(len(self.pesos_x)):
-			self.xp += self.pesos_x[i] * self.Xs[i] # K^k|k-1 = SUMATORIA ( Ws[i] * Xk|k[i] )
-			self.zp += self.pesos_x[i] * self.Zs[i]
-		self.Pp = 0
-		for i in range(len(self.pesos_p)):
-			self.Pp += self.pesos_p[i] * (self.Xs[i]-self.xp)*(self.Xs[i]-self.xp).T
-		self.Pp += self.Q
+    def setfx(self, mat):   # n x 1
+        self.fx = np.matrix(mat)
 
-	def actualizacion(self):
-		"""
-		Etapa de actualización
-		"""
-		self.Pyy = 0
-		self.Pxy = 0
-		for i in range(len(self.pesos_p)):
-			self.Pyy += self.pesos_p[i] * (self.Zs[i]-self.zp)*(self.Zs[i]-self.zp).T
-			self.Pxy += self.pesos_p[i] * (self.Xs[i]-self.xp)*(self.Zs[i]-self.zp).T
-		self.Pyy += self.R
-		self.G = self.Pxy*np.linalg.pinv(self.Pyy)  # Ganancia
-		self.x = self.xp + self.G*(self.z - self.zp)
-		self.P = self.Pp - self.G*self.Pyy*self.G.T
+    def sethx(self, mat):   # m x 1
+        self.hx = np.matrix(mat)
 
-	def iteracion(self, z, funcion_actualizacion, *args):
-		"""
-		Realizar una iteración => {
-			1.- Cálculo de los puntos sigma.
-			2.- Predicción.
-			3.- Actualización.
-		}
-		"""
-		self.actualizar_sigmas(funcion_actualizacion, *args)
-		self.z = np.matrix(z)
-		self.prediccion()
-		self.actualizacion()
-		return 0
+    def setP(self, mat):    # n x n
+        self.P = np.matrix(mat)
 
-	def get_filtrada(self):
-		"""
-		Obtener la filtrada del estado
-		"""
-		return self.x
+    def setQ(self, mat):    # n x n
+        self.Q = np.matrix(mat)
 
-	def get_prediccion(self):
-		"""
-		Obtener la predicción
-		"""
-		return self.xp
+    def setR(self, mat):    # m x m
+        self.R = np.matrix(mat)
 
-	def get_matriz_covarianza(self):
-		"""
-		Obtener la matriz de covarianza Px
-		"""
-		return self.P
+    def setAlpha(self, val):
+        self.alpha = val
 
-	def get_Ganancia(self):
-		"""
-		Obtener la ganacia de Kalman
-		"""
-		return self.G
+    def setBeta(self, val):
+        self.beta = val
+
+    def setKappa(self, val):
+        self.kappa = val    
+
+class ukf(model):
+
+    def __init__(self):
+        model.__init__(self)
+
+    def sigma(self):
+        points = []
+        self.weights_x = []
+        self.weights_P = []
+        L = self.x.shape[0]
+        lam = (self.alpha**2)*(L+self.kappa) - L
+        points.append(self.x)
+        add_val = scipy_alg.sqrtm((L + lam) * self.P)
+        for i in range(add_val.shape[1]):
+            add_val_mat = np.zeros(self.P.shape[0])
+            for j in range(add_val.shape[0]):
+                add_val_mat[j] = add_val[j][i]
+            add_val_mat = np.matrix(add_val_mat).T
+            points.append(self.x + add_val_mat)
+        for i in range(add_val.shape[1]):
+            add_val_mat = np.zeros(self.P.shape[0])
+            for j in range(add_val.shape[0]):
+                add_val_mat[j] = add_val[j][i]
+            add_val_mat = np.matrix(add_val_mat).T
+            points.append(self.x - add_val_mat)
+        self.weights_x.append(lam/(L+lam))
+        self.weights_P.append((lam/(L+lam)) + (1-self.alpha**2+self.beta))
+        for i in range(2*L):
+            self.weights_x.append(1/(2*(L+lam)))
+            self.weights_P.append(1/(2*(L+lam)))
+        self.xs = points
+
+    def update_sigma(self, func, *args):
+        self.sigma()
+        self.Xs = []
+        self.Zs = []
+        for self.i in range(len(self.xs)):
+            Xs_tem, Zs_tem = func(*args)
+            self.Xs.append(np.matrix(Xs_tem))
+            self.Zs.append(np.matrix(Zs_tem))
+
+    def predict(self):
+        self.xp = 0
+        self.zp = 0
+        for i in range(len(self.weights_x)):
+            self.xp += self.weights_x[i] * self.Xs[i]
+            self.zp += self.weights_x[i] * self.Zs[i]
+        self.Pp = 0
+        for i in range(len(self.weights_P)):
+            self.Pp += self.weights_P[i] * (self.Xs[i]-self.xp)*(self.Xs[i]-self.xp).T
+        self.Pp += self.Q
+        
+    def correct(self):
+        self.Pyy = 0
+        self.Pxy = 0
+        for i in range(len(self.weights_P)):
+            self.Pyy += self.weights_P[i] * (self.Zs[i]-self.zp)*(self.Zs[i]-self.zp).T
+            self.Pxy += self.weights_P[i] * (self.Xs[i]-self.xp)*(self.Zs[i]-self.zp).T
+        self.Pyy += self.R
+        self.G = self.Pxy*np.linalg.pinv(self.Pyy)
+        self.x = self.xp + self.G*(self.z - self.zp)
+        self.P = self.Pp - self.G*self.Pyy*self.G.T
+
+    def step(self, z, update_func, *args):
+        self.update_sigma(update_func, *args)
+        self.z = np.matrix(z)
+        self.predict()
+        self.correct()
+        return 0
+
+    def getX(self):
+        return self.x
+
+    def getP(self):
+        return self.P
+
+    def getG(self):
+        return self.G
