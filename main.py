8a982a43d532dca839053f57f50718bba8b1d0ea
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-12-09 20:31:01 -0600

    Ya predice *dab*

--- main.py
+++ main.py
@@ -4,150 +4,128 @@
 # Universidad Politécnica de Chiapas
 # Creado por Luis Fernando Hernández Morales y David Pérez Sánchez
 from utils.Unscented import ukf
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 
-def transformada(t):
-	"""
-	Calcular la función de transición del estado ƒ(x),
-	al mismo la función de observación h(x).
-	
-	Es importante definir la función de transición y de observación.
-	`Una de ellas puede ser lineal y la otra no lineal.`
-	Parámetros:
-	No requiere de parámetros.
-	"""
-	fx = [
-		[_ukf.xs[_ukf.i][0,0] + t * _ukf.xs[_ukf.i][1,0]], # Posición en X
-		[_ukf.xs[_ukf.i][1,0]],						   # Velocidad en X
-		[_ukf.xs[_ukf.i][2,0] + t * _ukf.xs[_ukf.i][3,0]], # Posición en Y
-		[_ukf.xs[_ukf.i][3,0]]						   # Velocidad en Y
-	]
-	hx = [
-		[math.sqrt(_ukf.xs[_ukf.i][0,0]**2 + _ukf.xs[_ukf.i][2,0]**2)], # Distancia del radar
-		[math.atan2(_ukf.xs[_ukf.i][2,0],_ukf.xs[_ukf.i][0,0])]			# Ángulo del radar
-	]
+def preparar_algoritmo():
+	# Matriz del estado
+	ukf.setX([[-2900],
+			  [80],
+			  [950],
+			  [20]])
+
+	# Matriz de covarianzas
+	ukf.setP([[100,0,0,0],
+			  [0,100,0,0],
+			  [0,0,100,0],
+			  [0,0,0,100]])
+
+	 # Ruido para la predicción
+	ukf.setQ([[0,0,0,0],
+			  [0,0.1,0,0],
+			  [0,0,0,0],
+			  [0,0,0,0.1]])
+
+	# Ruido para la actualización
+	ukf.setR([[50**2,0],
+			  [0,0.005**2]])
+
+	ukf.setAlpha(0.001)
+	ukf.setBeta(2)
+	ukf.setKappa(0)
+
+def funciones_fx_hx(t):
+	fx = [[ukf.xs[ukf.i][0,0] + t * ukf.xs[ukf.i][1,0]],
+		  [ukf.xs[ukf.i][1,0]],
+		  [ukf.xs[ukf.i][2,0] + t * ukf.xs[ukf.i][3,0]],
+		  [ukf.xs[ukf.i][3,0]]]
+	hx = [[math.sqrt(ukf.xs[ukf.i][0,0]**2 + ukf.xs[ukf.i][2,0]**2)],
+		  [math.atan2(ukf.xs[ukf.i][2,0],ukf.xs[ukf.i][0,0])]]
 	return fx, hx
 
-"""
-Variables iniciales para el estado
-"""
-posicion_x_inicial = -2900
-posicion_y_inicial = 950
-velocidad_x_inicial = 80
-velocidad_y_inicial = 20
+ukf = ukf()
+preparar_algoritmo()
 
-ruido_p = 100
-ruido_q = 0.1
-ruido_R = 50
-ruido_V = 0.005
+time = np.arange(0, 60, 0.1)
+x = np.linspace(-3000,3000,len(time))
+y = np.linspace(1000,800,len(time))
+xv = np.gradient(x,time)
+yv = np.gradient(y,time)
+r_raw = np.sqrt(x**2+y**2)
+theta_raw = np.arctan2(y,x)
+r_noise = np.random.normal(0,50,len(r_raw))
+theta_noise = np.random.normal(0,0.005,len(theta_raw))
+r = r_raw + r_noise
+theta = theta_raw + theta_noise
 
-instantes_tiempo = 60
+prev = 0
+x_f = []
+y_f = []
+xv_f = []
+yv_f = []
+x_p = []
+y_p = []
+xv_p = []
+yv_p = []
+for i in range(len(time)):
+	t = float(time[i]) - prev
+	prev = float(time[i])
+	z = [[r[i]], # Observaciones del sensor
+		 [theta[i]]]
+	ukf.step(z, funciones_fx_hx, t) # Proceso de puntos sigmas, predicción y actualización
+	x_filtered = ukf.getX() # Filtrada del estadp
+	p_filtered = ukf.getP() # Covarianzas
+	g_filtered = ukf.getG() # Ganancia de Kalman
+	prediction = ukf.xp # Predicción del estado
+	
+	x_p.append(prediction[0,0])
+	y_p.append(prediction[2,0])
+	xv_p.append(prediction[1,0])
+	yv_p.append(prediction[3,0])
 
-estado = [
-	[posicion_x_inicial], # Posición en X
-	[velocidad_x_inicial],	 # Velocidad en X
-	[posicion_y_inicial],   # Posición en Y
-	[velocidad_y_inicial]	 # Velocidad en Y
-]
+	x_f.append(x_filtered[0,0])
+	xv_f.append(x_filtered[1,0])
+	y_f.append(x_filtered[2,0])
+	yv_f.append(x_filtered[3,0])
 
-P = [
-	[ruido_p, 0, 0, 0],
-	[0, ruido_p, 0, 0],
-	[0, 0, ruido_p, 0],
-	[0, 0, 0, ruido_p]
-] # nx1
+x_f = np.array(x_f)
+xv_f = np.array(xv_f)
+y_f = np.array(y_f)
+yv_f = np.array(yv_f)
+x_p = np.array(x_p)
+y_p = np.array(y_p)
+xv_p = np.array(xv_p)
+yv_p = np.array(yv_p)
 
-Q = [
-	[0, 0, 0, 0],
-	[0, ruido_q, 0, 0],
-	[0, 0, 0, 0],
-	[0, 0, 0, ruido_q]
-] # nx1
-
-R = [
-	[np.random.normal(0, ruido_R**2), 0],
-	[0, np.random.normal(0, ruido_V**2)]
-] #mxm => Tamaño de las observaciones
-
-"""
-Inicializar la clase del algoritmo de kalman unscented
-"""
-_ukf = ukf()
-_ukf.setX(estado)
-_ukf.setP(P)
-_ukf.setQ(Q)
-_ukf.setR(R)
-_ukf.setAlpha(0.001)
-_ukf.setBeta(2)
-_ukf.setKappa(0)
-tiempos = np.arange(0, instantes_tiempo, 0.1)
-tiempo_anterior = 0
-
-# Fijar los límites de la gráfica
-x = np.linspace(-3000,3000,len(tiempos))
-y = np.linspace(1000,800,len(tiempos))
-r = np.sqrt(x**2+y**2) # Medida
-theta = np.arctan2(y,x) # Medida
-ruido_r = np.random.normal(0,ruido_R,len(r))
-ruido_theta = np.random.normal(0,ruido_V,len(theta))
-vy = np.gradient(y,tiempos) # Dinámica de la velocidad en X
-vx = np.gradient(x,tiempos) # Dinámica de la velocidad en Y
-_r = r + ruido_r
-_theta = theta + ruido_theta
-
-posicion_x_filtrada = []
-posicion_y_filtrada = []
-velocidad_x_filtrada = []
-velocidad_y_filtrada = []
-
-for i in range(len(tiempos)):
-	t = tiempos[i]-tiempo_anterior
-	tiempo_anterior = tiempos[i]
-	z = [
-		[_r[i]], # Observación del sensor => distancia
-		[_theta[i]] # Observación del sensor => distancia
-	]
-	_ukf.step(z, transformada, t)
-	x_filtrada = _ukf.getX()
-	posicion_x_filtrada.append(x_filtrada[0,0])
-	velocidad_x_filtrada.append(x_filtrada[1,0])
-	posicion_y_filtrada.append(x_filtrada[2,0])
-	velocidad_y_filtrada.append(x_filtrada[3,0])
-
-posicionesX = np.array(posicion_x_filtrada)
-posicionesY = np.array(posicion_y_filtrada)
-velocidadesX = np.array(velocidad_x_filtrada)
-velocidadesY = np.array(velocidad_y_filtrada)
-
-"""
-Graficar
-"""
 
 plt.subplot(411)
-plt.plot(tiempos,x,'b',label='Truth')
-plt.plot(tiempos,posicion_x_filtrada,'r',label='UKF')
-plt.title('X position')
+plt.plot(time,x,'b',label='Real')
+plt.plot(time,x_f,'r',label='Filtrada')
+plt.plot(time,x_p,'y-',label='Predicha')
+plt.title('Posiciones en X')
 plt.xticks([])
 plt.subplot(412)
-plt.plot(tiempos,vx,'b',label='Truth')
-plt.plot(tiempos,velocidad_x_filtrada,'r',label='UKF')
-plt.title('X velocity')
+plt.plot(time,xv,'b',label='Real')
+plt.plot(time,xv_f,'r',label='Filtrada')
+plt.plot(time,xv_p,'y-',label='Predicha')
+plt.title('Velocidades en X')
 plt.xticks([])
 plt.subplot(413)
-plt.plot(tiempos,y,'b',label='Truth')
-plt.plot(tiempos,posicion_y_filtrada,'r',label='UKF')
-plt.title('Y position')
+plt.plot(time,y,'b',label='Real')
+plt.plot(time,y_f,'r',label='Filtrada')
+plt.plot(time,y_p,'y-',label='Predicha')
+plt.title('Posiciones en Y')
 plt.xticks([])
 plt.subplot(414)
-plt.plot(tiempos,vy,'b',label='Truth')
-plt.plot(tiempos,velocidad_y_filtrada,'r',label='UKF')
-plt.title('Y velocity')
+plt.plot(time,yv,'b',label='Real')
+plt.plot(time,yv_f,'r',label='Filtrada')
+plt.plot(time,yv_p,'y-',label='Predicha')
+plt.title('Velocidades en Y')
 plt.legend()
-plt.figure()
-plt.plot(x,y,'b',label='Truth')
-plt.plot(posicion_x_filtrada,posicion_y_filtrada,'r',label='UKF')
-plt.title('XY Position')
+plt.plot(x,y,'b',label='Real')
+plt.plot(x_f,y_f,'r',label='Filtrada')
+plt.plot(x_p,y_p,'r',label='Predicha')
+plt.title('Trayectoria del avión')
 plt.legend()
 plt.show()
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 # Diseño de sistemas inteligentes - 10º A
 
+<p align="center">
+	<img src="https://i.ibb.co/PtV87CY/descripcion-problema.png" alt="Descripción del problema" width="400" height="250">
+</p>
+
 ##### Descripción del problema
 
-> Un radar se sitúa en una coordenada y rastrea un avión que se encuentra en una posición en un plano 2D
+Un radar se sitúa en una coordenada y rastrea un avión que se encuentra en una posición en un plano 2D.
+
+Dicho radar se encuentra realizando observaciones en un instante de tiempo __Δt__, en cada observación, el radar devuelve la distancia aproximada a la que se encuentra el avión y un ángulo &theta; de inclinación.
+
+##### Dinámica
+
